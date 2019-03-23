3fd87fc5403d3b446716ad8ca07dce6dee6fa080
przemo06133 <przemo06133@gmail.com>
2019-03-23 21:57:40 +0100

    Enum field and more test improved

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -6,10 +6,16 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = typeOfAccount;
     }
 
     public double deposit(double amount, boolean branch){
@@ -25,4 +31,9 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
+    }
+
 }
